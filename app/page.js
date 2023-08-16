6c610e7c0995836452ab927c7f1c30f1f3d9919b
Drabsenior <drabsenior@gmail.com>
2023-08-16 16:13:17 +0300

    finished background particle

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@ import { fadeIn } from "@/Variants";
 import Pagewrapper from "./components/Pagewrapper";
 import ProjectsBtn from "./components/ProjectsBtn";
 import { motion } from "framer-motion";
+import Avatar from "./components/Avatar";
+import ParticleContainer from "./components/ParticleContainer";
 export default function Home() {
   return (
     <Pagewrapper>
@@ -52,7 +54,20 @@ export default function Home() {
         <div>image</div>
         <div className="w-[1200px] h-full absolute right-0 bottom-0">
           {/* bg-imate */}
-          <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right"></div>
+          <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+          {/* particles */}
+          <ParticleContainer />
+          {/* avatar */}
+          <motion.div
+            className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:right-[8%] lg:bottom-0"
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            transition={{ duration: 2, ease: "easeInOut" }}
+          >
+            <Avatar />
+          </motion.div>
         </div>
       </div>
     </Pagewrapper>
