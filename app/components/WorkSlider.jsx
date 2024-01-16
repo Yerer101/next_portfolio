c7ccc3ebd44148e0208c4e52d5e95d675fd879fe
Drabsenior <drabsenior@gmail.com>
2024-01-16 18:13:07 +0300

    added a link

--- app/components/WorkSlider.jsx
+++ app/components/WorkSlider.jsx
@@ -13,51 +13,33 @@ import Link from "next/link";
 
 const workSlides = {
   slides: [
-    {
-      images: [
-        {
-          title: "title",
-          path: "/thumb1.jpg",
-        },
-        {
-          title: "title",
-          path: "/thumb2.jpg",
-        },
-        {
-          title: "title",
-          path: "/thumb3.jpg",
-        },
-        {
-          title: "title",
-          path: "/thumb4.jpg",
-        },
-      ],
-    },
+    
+    
     {
       images: [
         {
           title: "title",
           path: "/thumb4.jpg",
           source: "",
-          link: "",
+          link: "https://kezyasv2.onrender.com/",
         },
         {
           title: "title",
           path: "/thumb1.jpg",
           source: "",
-          link: "",
+          link: "https://plusova.com/",
         },
         {
           title: "title",
           path: "/thumb2.jpg",
           source: "",
-          link: "",
+          link: "https://imdb-oh67.vercel.app/",
         },
         {
           title: "title",
           path: "/thumb3.jpg",
           source: "",
-          link: "",
+          link: "https://iat-appointment.onrender.com/",
         },
       ],
     },
@@ -90,12 +72,12 @@ const WorkSlider = () => {
                       {/* overlay gradient */}
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-70 transition-all duration-700"></div>
                       {/* links to source and web */}
-                      <Link href={image.source ? image.source : ""}>
+                      <Link href={image.source ? image.source : ""} target="_blank" rel="noopener noreferrer">
                         <h2 className="absolute bottom-0 -translate-y-[900%] group-hover:translate-y-44 group-hover:xl:-translate-y-44 right-4 duration-700 tracking-wide font-mono  hover:text-red-600 hover:scale-125 hover:font-bold transition-all ">
                           Source
                         </h2>
                       </Link>
-                      <Link href={image.link ? image.link : ""}>
+                      <Link href={image.link ? image.link : ""} target="_blank" rel="noopener noreferrer">
                         <h2 className="absolute bottom-0 -translate-y-[900%] group-hover:translate-y-44 group-hover:xl:-translate-y-44 right-24 duration-300 tracking-widest font-mono  hover:text-red-600 hover:scale-125 hover:font-bold transition-all">
                           Link
                         </h2>
